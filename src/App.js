6f6696c1a05674fee468addf6bac216c0078de4a
lboyadzhiev <l.boyadzhievv@abv.bg>
2022-01-11 12:20:48 +0200

    start portfolio with React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='app'>
+            <Header />
+        </div>
+    );
+}
+
+function Header() {
+    return (
+        <div className='header'>
+            <Logo />
+            <Navigation />
+        </div>
+    );
+}
+
+function Logo() {
+    return (
+        <div className='logo'>
+            <div className='name'>
+                <a href='#'>
+                    <h3 className='heading-3'>Lachezar Boyadzhiev</h3>
+                    <span className='subheading'>Front End Developer</span>
+                </a>
+            </div>
+        </div>
+    );
+}
+function Navigation() {
+    return (
+        <nav className='nav'>
+            <ul>
+                <li>
+                    <a href='#'>About</a>
+                </li>
+                <li>
+                    <a href='#'>Ecperience</a>
+                </li>
+                <li>
+                    <a href='#'>Projects</a>
+                </li>
+                <li>
+                    <a href='#'>Skills</a>
+                </li>
+                <li>
+                    <a href='#'>Education</a>
+                </li>
+                <li>
+                    <a href='#'>Awards</a>
+                </li>
+                <li>
+                    <a href='#'>Resume</a>
+                </li>
+            </ul>
+        </nav>
+    );
 }
 
 export default App;
