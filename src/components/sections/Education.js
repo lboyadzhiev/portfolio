a9bd6dd60ee5b46101f44df6fd3ea00be7317197
lboyadzhiev <l.boyadzhievv@abv.bg>
2022-01-25 09:37:55 +0200

    fix responsive skills section

--- src/components/sections/Education.js
+++ src/components/sections/Education.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
 import Heading from '../utilities/SectionHeading';
+import Item from '../utilities/Item';
 
 const Education = () => {
     return (
         <div className='section-education'>
             <Heading name='Education' />
+            <Items className='education__item' />
         </div>
     );
 };
 
 export default Education;
+
+const Items = () => {
+    return (
+        <div className='education'>
+            <Item className='education__item' />
+            <Item className='education__item' />
+        </div>
+    );
+};
---
+++ src/components/utilities/Item.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Item(props) {
+    return (
+        <div className={props.className}>
+            <div className='item__heading'>
+                <h3 className='item__name'>Soft Uni</h3>
+                <h4 className='heading-4'>JS Fundantals</h4>
+            </div>
+            <div className='item__description'>
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default Item;
