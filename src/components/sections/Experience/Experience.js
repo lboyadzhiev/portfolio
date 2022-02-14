be436d0285c3ca5376218fd65a95b77feb51d79b
lboyadzhiev <l.boyadzhievv@abv.bg>
2022-02-14 10:22:55 +0200

    Experience folder

---
+++ src/components/sections/Experience/Experience.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Heading from '../../utilities/SectionHeading';
+import Items from './Items';
+
+const Experience = () => {
+    return (
+        <div className='experience'>
+            <Heading name='Experience' />
+            <Items />
+        </div>
+    );
+};
+
+export default Experience;
---
+++ src/components/sections/Experience/Items.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Item from '../../utilities/Item';
+
+const Items = () => {
+    return (
+        <div className='experience__list'>
+            <Item className='experience__item' />
+        </div>
+    );
+};
+
+export default Items;
